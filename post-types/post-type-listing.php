bf6d4ba70df3acf0064b934a681263593d171601
Donna McMaster <donna@donnamcmaster.com>
2017-04-14 23:40:32 -0700

    rewrite directory listings displays
    - remove code to write to files and display from files
    - rewrite shortcode to generate displays on the fly
    - rewrite shortcode to use output buffer instead of string concatenation
    - change order so that prem with menu_order 0 are displayed randomly and all other listings are displayed in menu_order (per CLCC changes for 2017)

--- post-types/post-type-listing.php
+++ post-types/post-type-listing.php
@@ -119,7 +119,6 @@ function __construct ( ) {
 
 protected function init_filters_and_actions () {
 	add_shortcode( 'dir_list', array( $this, 'dir_list' ) );
-	add_action( 'save_post', array( $this, 'write_directory_files' ) );
 }
 
 
@@ -130,42 +129,121 @@ protected function init_filters_and_actions () {
 /**
  *	Shortcode: dir_listings
  *	[dir_list cat="food-2"]
- *		include category in file food-2.inc, with no headline
+ *		include category with slug food-2, with no headline
  *	[dir_list cat="food-2" head="Local Restaurants"]
- *		include category in file food-2.inc, with h2 headline
+ *		include category with slug food-2, with h2 headline
  *	[dir_list] or [dir_list cat="*"] or [dir_list cat="directory"]
  *		include all categories, with jumplinks at the top and <h2> for each cat
  */
-function dir_list( $atts ) {
-	// initialize return value
-	$s = '';
+public function dir_list( $atts ) {
+	// pick up parameters
 	extract( shortcode_atts( array(
 		'cat' => '*',
 		'head' => '',
 	), $atts ) );
 
-	// open local files with fopen()
-	if ( $cat == '*' ) {
-		$cat = 'directory';
-	}
-	$fname = INCLUDES_PATH . $cat . '.inc';
-	if ( file_exists( $fname) ) {
-		$handle = fopen( $fname, 'r' );
-		$s = $head ? "<h2>$head</h2>" : '';
-		$s .= fread( $handle, filesize( $fname ) );
-		fclose( $handle );
+	// get all categories? 
+	if ( ( $cat == '*' ) || ( $cat == 'directory' ) ) {
+		// pull requested info into output buffer
+		ob_start();
+
+	    $cats_list = get_terms( 'dir_cat' );
+		echo $this->get_jump_links( $cats_list );
+		reset( $cats_list );
+
+		foreach ( $cats_list as $catobj ) {
+			echo $this->get_cat_listing( $catobj, true );
+		}
+		return ob_get_clean();
+
 	} else {
-		mcw_log( "mcw_include: local file $fname not found" );
+		$catobj = get_term_by( 'slug', $cat, 'dir_cat' );
+		if ( $catobj ) { 
+			return $this->get_cat_listing( $catobj, $head );
+		} else {
+			mcw_log( "McPik_Post_Type_Listing::dir_list > can't find $cat" );
+			return '';
+		}
 	}
-	return $s;
 }
 
-function get_meta_item ( $item, $list ) {
+private function get_meta_item ( $item, $list ) {
 	return array_key_exists( $item, $list ) ? $list[$item] : '';
 }
 
-function get_cat_listing ( $catobj ) {
-	// get the listings
+private function get_listing ( $listing ) {
+	ob_start();
+	$meta = McPik_Utils::get_simple_post_custom( $listing->ID );
+	$_mcw_listing_alt_title = $this->get_meta_item( '_mcw_listing_alt_title', $meta );
+	$_mcw_listing_type = $this->get_meta_item( '_mcw_listing_type', $meta );
+	$_mcw_listing_url = $this->get_meta_item( '_mcw_listing_url', $meta );
+	$_mcw_listing_phone = $this->get_meta_item( '_mcw_listing_phone', $meta );
+	$_mcw_listing_email = $this->get_meta_item( '_mcw_listing_email', $meta );
+
+	$title = $_mcw_listing_alt_title ? $_mcw_listing_alt_title : $listing->post_title;
+
+	// set defaults for basic, then pick up URL & descr for others
+	$prefix = $suffix = '';
+	if ( ( $_mcw_listing_type != 'bas' ) && $_mcw_listing_url ) {
+		$prefix = '<a href="' . $_mcw_listing_url . '" target="_blank">';
+		$suffix = '</a>';
+	}
+	$email = $_mcw_listing_email ? ' ' . McPik_Utils::get_anchor( "mailto:$_mcw_listing_email", $_mcw_listing_email ) : '';
+	if ( $_mcw_listing_type != 'bas' ) {
+		$descr = BR . wptexturize( $listing->post_content ) . $email . ' ' . $_mcw_listing_phone;
+	} else {
+		$descr = ' - ' . wptexturize( $listing->post_content ) . $email . ' ' . $_mcw_listing_phone;
+	}
+
+	if ( $_mcw_listing_type == 'prem' ) {
+		$image = get_the_post_thumbnail( $listing->ID, 'listing-img' );
+		echo '<li class="prem_listing cf"><div class="list_img">' . $image .'</div><div class="list_body"><strong>' . $prefix . $title . $suffix . '</strong> ';
+	} else {
+		echo '<li><div class="list_body"><strong>' . $prefix . $title . $suffix . '</strong>';
+	}
+	echo $descr . '</div></li>' . PHP_EOL;
+	return ob_get_clean();
+}
+
+
+/*
+	$heading:
+		false or null value => don't display any heading
+		boolean true => display cat name
+		any other value => display that value
+*/
+private function get_cat_listing ( $catobj, $heading=false ) {
+
+	ob_start();
+
+	if ( $heading === true ) {
+		$heading = $catobj->name;
+	}
+	if ( $heading ) {
+?>
+	<h2 id="dircat_<?= $catobj->term_id; ?>"><?= $heading; ?></h2>
+
+<?php
+	}
+
+?>
+	<ul class="dir_listings">
+
+<?php
+	// get the premium listings with priority 0
+	$args = array (
+		'post_type' => 'listing',
+		'menu_order' => 0,
+		'order' => 'rand',
+		'numberposts' => -1,
+		'dir_cat' => $catobj->slug,
+	);
+	$listings = get_posts( $args );
+	foreach ( $listings as $listing ) {
+		echo $this->get_listing( $listing );
+	}
+
+	// get the remaining listings
 	$args = array (
 		'post_type' => 'listing',
 		'orderby' => $this->default_sort_by,
@@ -174,81 +252,20 @@ function get_cat_listing ( $catobj ) {
 		'dir_cat' => $catobj->slug,
 	);
 	$listings = get_posts( $args );
-	$s = '<ul class="dir_listings">' . PHP_EOL;
-
 	foreach ( $listings as $listing ) {
-		$meta = mcw_get_simple_post_custom( $listing->ID );
-		$_mcw_listing_alt_title = $this->get_meta_item( '_mcw_listing_alt_title', $meta );
-		$_mcw_listing_type = $this->get_meta_item( '_mcw_listing_type', $meta );
-		$_mcw_listing_url = $this->get_meta_item( '_mcw_listing_url', $meta );
-		$_mcw_listing_phone = $this->get_meta_item( '_mcw_listing_phone', $meta );
-		$_mcw_listing_email = $this->get_meta_item( '_mcw_listing_email', $meta );
-
-		$title = $_mcw_listing_alt_title ? $_mcw_listing_alt_title : $listing->post_title;
-
-		// set defaults for basic, then pick up URL & descr for others
-		$prefix = $suffix = '';
-		if ( ( $_mcw_listing_type != 'bas' ) && $_mcw_listing_url ) {
-			$prefix = '<a href="' . $_mcw_listing_url . '" target="_blank">';
-			$suffix = '</a>';
+		if ( $listing->menu_order > 0 ) { 
+			echo $this->get_listing( $listing );
 		}
-		$email = $_mcw_listing_email ? ' ' . mcw_get_mailto ( $_mcw_listing_email, $_mcw_listing_email ) : '';
-		if ( $_mcw_listing_type != 'bas' ) {
-			$descr = BR . apply_filters( 'the_content', $listing->post_content . $email . ' ' . $_mcw_listing_phone );
-		} else {
-			$descr = ' - ' . wptexturize( $listing->post_content ) . $email . ' ' . $_mcw_listing_phone;
-		}
-
-		if ( $_mcw_listing_type == 'prem' ) {
-			$image = get_the_post_thumbnail( $listing->ID, 'listing_img' );
-			$s .= '<li class="prem_listing cf"><div class="list_img">' . $image .'</div><div class="list_body"><strong>' . $prefix . $title . $suffix . '</strong> ';
-		} else {
-			$s .= '<li><div class="list_body"><strong>' . $prefix . $title . $suffix . '</strong>';
-		}
-		$s .= $descr . '</div></li>' . PHP_EOL;
 	}
-	$s .= '</ul>' . PHP_EOL;
-	return $s;
+?>
+	</ul>
+
+<?php
+	return ob_get_clean();
 }
 
 
-/**
- *	Write Directory Files:
- *	called whenever a post is saved (hook save_post)
- *	if this is an updated listing, then we write a new set of files to INCLUDES_PATH
- *	files written include "directory.inc" (all listings, jump links, headers) plus
- *		one listings-only file for each cat, named "<cat_name>.inc"
- *	future optimization: write only the file(s) for the cat(s) containing this listing
- *	note that in spite of the opt, we would still need to parse all listings/cats
- *		in order to create the full directory file
- *	creates a lock file for the duration of the writes to prevent conflict
- *	to avoid browsers picking up partial listings, new files are written as "<fname>.tmp"; 
- *		once a file is complete, the "<fname>.inc" is deleted and the tmp file is renamed 
- */
-
-function write_directory_files ( $post_id ) {
-	// if it's a revision or the wrong post type, we're not interested
-	if ( wp_is_post_revision( $post_id ) || ( $this->post_type != get_post_type( $post_id ) ) ) {
-        return;
-    }
-    
-    // use lock file to prevent access conflict
-    if ( file_exists( INCLUDES_LOCK_FILE ) ) {
-    	mcw_log( "write_directory_files: access conflict" );
-    	return;
-    }
-    $lock_handle = fopen( INCLUDES_LOCK_FILE, 'w' ) or die( 'Cannot open file:  '.INCLUDES_LOCK_FILE );
-    fclose( $lock_handle );
-
-	// open a temporary file for the entire directory
-	$temp_dir_fname = INCLUDES_PATH . 'directory.tmp';
-	$dir_fname = INCLUDES_PATH . 'directory.inc';
-    if ( file_exists( $temp_dir_fname ) ) {
-		unlink( $temp_dir_fname );
-	}
-    $dir_handle = fopen( $temp_dir_fname, 'w' ) or die( 'Cannot open file:  '.$temp_dir_fname );
-
-    $cats_list = get_terms( 'dir_cat' );
+private function get_jump_links ( $cats_list ) {
 
 	// jump links for full directory listing
 	$nr_cats = count( $cats_list );
@@ -257,66 +274,40 @@ function write_directory_files ( $post_id ) {
 	if ( ( $nr_cats % $nr_cols) <> 0 ) {
 		$nr_rows = ceil( $nr_rows );
 	}
-	settype( $nr_rows,'int' );
+	settype( $nr_rows, 'int' );
 
-	// print jumplinks
-	$s = '<div id="jump_links" class="cf">' . PHP_EOL . '<ul id="jump_1" class="jump-links">';
+	ob_start();
+?>
+	<div id="jump_links" class="cf">
+		<ul id="jump_1" class="jump-links">
+<?php
 	$cat_count = 1;
 	$col = 1;
 	foreach ( $cats_list as $catobj ) {
 		if ( ( $col == 1 ) && ( $cat_count > $nr_rows ) ) {
-			$s .=  '</ul>' . PHP_EOL . '<ul id="jump_2" class="jump-links">';
 			$col++;
+?>
+		</ul>
+		<ul id="jump_2" class="jump-links">
+<?php
 		}
-		$s .= PHP_EOL . '<li>' . mcw_get_anchor( '#dircat_'.$catobj->term_id, $catobj->name ) . '</li>';
+?>
+			<li><?= McPik_Utils::get_anchor( '#dircat_'.$catobj->term_id, $catobj->name ); ?></li>
+<?php
 		$cat_count++;
 	}
-	$s .=  '</ul></div>' . PHP_EOL;
-	fwrite( $dir_handle, $s );
-
-	// each cat is written to the directory and to an individual file
-	reset( $cats_list );
-	foreach ( $cats_list as $catobj ) {
-		$this->write_cat_to_files( $catobj, $dir_handle );
-	}
+?>
+		</ul>
+	</div>
+<?php
 	
-	// close temp directory file; delete old .inc file and rename .tmp to .inc
-    fclose( $dir_handle );
-    if ( file_exists( $dir_fname ) ) {
-		unlink( $dir_fname );
-	}
-	rename( $temp_dir_fname, $dir_fname);
-
-	// must remove the lock file!
-	unlink( INCLUDES_LOCK_FILE );
-}
-
-function write_cat_to_files ( $catobj, $dir_handle ) {
-	$tempname = INCLUDES_PATH . $catobj->slug . '.tmp';
-	$fname = INCLUDES_PATH . $catobj->slug . '.inc';
-    if ( file_exists( $tempname ) ) {
-		unlink( $tempname );
-	}
-    $cat_handle = fopen( $tempname, 'w' ) or die( 'Cannot open file:  '.$tempname );
-
-	fwrite( $dir_handle, PHP_EOL . '<h2 id="dircat_' . $catobj->term_id . '">' . $catobj->name . '</h2>' . PHP_EOL );
-	$cat_string = $this->get_cat_listing( $catobj );
-	fwrite( $dir_handle, $cat_string );
-	fwrite( $cat_handle, $cat_string );
-
-	// close the temp file; delete the previous file; rename temp
-    fclose( $cat_handle );
-    if ( file_exists( $fname ) ) {
-		unlink( $fname );
-	}
-	rename( $tempname, $fname);
+	return ob_get_clean();
 }
 
 
 /**
  *	Admin Methods
  */
-
 
 public function manage_columns ( $defaults ) {
     unset( $defaults['comments'] );
